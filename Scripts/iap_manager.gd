e6b11e3c4c681099e154b1db9952dfd78fc27872
StatiCPC <joshorellana73@gmail.com>
2025-07-31 00:34:46 -0600

    Se agrego el plugin google play billing para manejo de compras dentro de la aplicacion, se configuro una escena IAPManager, para manejar toda la logica de las compras internas y se configuro una keystore para la exportacion del juego

--- Scripts/iap_manager.gd
+++ Scripts/iap_manager.gd
@@ -1,6 +1,19 @@
 extends Node
 
 signal unlock_new_skin
+var google_payment = null
+
+func _ready():
+	if Engine.has_singleton("GodotGooglePlayBilling"):
+		var google_payment = Engine.get_singleton("GodotGooglePlayBilling")
+		MyUtility.add_log_msg("Android IAP support is enabled")
+		google_payment.connected.connect(_on_connected)
+		google_payment.startConnection()
+	else:
+		MyUtility.add_log_msg("Android IAP support is not available")
 
 func purchase_skin():
 	unlock_new_skin.emit()
+
+func _on_connected():
+	MyUtility.add_log_msg("Connected")
